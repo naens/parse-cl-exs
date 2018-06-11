882b315c98dd88672e8fb89547dfa40df5ce059a
naens <naens@gmx.com>
2018-06-11 19:16:19 +0200

    add gram_reader

---
+++ src/gram_reader.rs
@@ -0,0 +1,12 @@
+// reads lexer and parser grammars
+pub mod gram_reader {
+
+    struct Rule {
+        // TODO: identifier
+        // TODO: expression tree
+    }
+    // reads a text file and outputs a list of rules
+    pub fn read (text: &str) -> Vec<rule> {
+
+    }
+}
